391e2d197473f795492d13f5c39765244de87e83
ddevarg <david.devargas@morningstar.com>
2023-03-21 10:03:19 -0500

    setup: remove v2 repo

--- remote/webpack.config.js
+++ remote/webpack.config.js
@@ -3,62 +3,62 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-  output: {
-    publicPath: "http://localhost:8081/",
-    uniqueName: "remoteApp",
-    scriptType: "text/javascript",
-  },
+    output: {
+        publicPath: "http://localhost:8081/",
+        uniqueName: "remoteApp",
+        scriptType: "text/javascript",
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".vue", ".jsx", ".js", ".json"],
-  },
+    resolve: {
+        extensions: [".tsx", ".ts", ".vue", ".jsx", ".js", ".json"],
+    },
 
-  devServer: {
-    port: 8081,
-    hot: true,
-    historyApiFallback: true,
-    allowedHosts: "all",
-  },
+    devServer: {
+        port: 8081,
+        hot: true,
+        historyApiFallback: true,
+        allowedHosts: "all",
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      {
-        test: /\.tsx?$/,
-        use: [
-          "babel-loader",
-          {
-            loader: "ts-loader",
-            options: {
-              transpileOnly: true,
-              appendTsSuffixTo: ["\\.vue$"],
-              happyPackMode: true,
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
             },
-          },
+            {
+                test: /\.tsx?$/,
+                use: [
+                "babel-loader",
+                {
+                    loader: "ts-loader",
+                    options: {
+                    transpileOnly: true,
+                    appendTsSuffixTo: ["\\.vue$"],
+                    happyPackMode: true,
+                    },
+                },
+                ],
+            },
+            {
+                test: /\.(css|s[ac]ss)$/i,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
         ],
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-    ],
-  },
+    },
 
-  plugins: [
-    new VueLoaderPlugin(),
-    new ModuleFederationPlugin({
-      name: "app2",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        "./Header": "./src/Header.vue",
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+    plugins: [
+        new VueLoaderPlugin(),
+        new ModuleFederationPlugin({
+            name: "app2",
+            filename: "remoteEntry.js",
+            remotes: {},
+            exposes: {
+                "./Header": "./src/Header.vue",
+            },
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
 };
